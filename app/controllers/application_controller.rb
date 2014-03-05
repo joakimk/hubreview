26391d7c41387a26918c65013ace234024e00f8e
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2014-03-05 20:51:33 +0100

    Better message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
     raise "Need AUTH_KEY configured in prod." unless ENV['AUTH_KEY']
 
     if !session[:authorized] && params[:auth_key] != ENV['AUTH_KEY']
-      render text: 'Not authorized, did you forget adding auth_key to the url?'
+      render text: "Not authorized, did you forget to add \"auth_key\" to the url?\n"
     else
       session[:authorized] = true
     end
