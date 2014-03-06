61449a2e49f241c10c003541bfb2961ee9dce899
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2014-03-06 22:15:59 +0100

    Cleanup spec.

--- spec/features/reviewing_spec.rb
+++ spec/features/reviewing_spec.rb
@@ -5,28 +5,39 @@ describe "Reviewing revisons" do
   it "can mark revisions as new"
 
   it "marks revisions as in review for a while when you click on them", :js do
-    revision = Revision.create! name: "abc123abc123abc123", branch: "master", author: "Joe", url: "http://example.org", message: "Message", payload: {}
+    revision = create_revision name: "abc123abc123abc123"
 
     visit "/"
 
-    expect(page).to have_content("abc123")
-    expect(page).not_to have_selector(".revision.in-review")
+    expect(page).not_to have_revision_in_review
 
     click_link "abc123"
 
-    visit "/" # normally it updates by websockets
-    expect(page).to have_selector(".revision.in-review")
+    update_page
+    expect(page).to have_revision_in_review
     expect(revision.reload.in_review_at).to be_within(3).of(Time.now)
   end
 
   it "does not mark reviewed revisions as in-review when you click on them", :js do
-    revision = Revision.create! name: "abc123abc123abc123", branch: "master", author: "Joe", url: "http://example.org", message: "Message", reviewed: true, payload: {}
+    revision = create_revision name: "abc123abc123abc123", reviewed: true
 
     visit "/"
 
     click_link "abc123"
 
+    update_page
+    expect(page).not_to have_revision_in_review
+  end
+
+  def update_page
     visit "/" # normally it updates by websockets
-    expect(page).not_to have_selector(".revision.in-review")
+  end
+
+  def have_revision_in_review
+    have_selector(".revision.in-review")
+  end
+
+  def create_revision(opts = {})
+    Revision.create!({ name: "abc123abc123abc123", branch: "master", author: "Joe", url: "http://example.org", message: "Message", payload: {} }.merge(opts))
   end
 end
