af3eb7cc00113af70b3d463ddfa555825a35534f
Henrik Nyh <henrik@nyh.se>
2014-03-22 17:11:18 +0100

    Use https for Gravatars.
    To fix mixed-content warnings.

--- lib/gravatar.rb
+++ lib/gravatar.rb
@@ -3,6 +3,6 @@ require "digest/md5"
 module Gravatar
   def self.url_from_email(email)
     hash = Digest::MD5.hexdigest(email.downcase)
-    "http://www.gravatar.com/avatar/#{hash}"
+    "https://www.gravatar.com/avatar/#{hash}"
   end
 end
